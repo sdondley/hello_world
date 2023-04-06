7a09cd9ee17a018ad3c029cbf396785621620b5e
Steve Dondley <s@dondley.com>
2023-04-06 12:48:51 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hello_world_steve",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         # Add any additional package dependencies here, if needed.
