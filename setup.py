4d48d620125702ceb8157f760234a2fe17bacab1
Steve Dondley <s@dondley.com>
2023-04-06 12:22:21 -0400

    rename module

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="hello_world",
+    name="hello_world_steve",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
