f8ec0c909529511e15f045241c3fa78945f91494
Steve Dondley <s@dondley.com>
2023-04-06 10:49:38 -0400

    Add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello_world",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add any additional package dependencies here, if needed.
+    ],
+)
+
